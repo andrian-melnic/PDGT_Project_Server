ca423254b096a195d235d0298c095cf8861f03e4
andrianMelnic29 <melnic.andry97@gmail.com>
2020-03-17 20:36:41 +0100

    Update DrinkingWater.js
    - New route for adding a new fountain

--- routes/DrinkingWater.js
+++ routes/DrinkingWater.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Location = require('../models/DrinkingWaterLocation')
 
 router.get('/', async(req, res) => {
+  // GET all drinking water fountains
   try{
     const drinkingWaterLocs = await (Location.find({}))
     res.send(drinkingWaterLocs)
@@ -10,10 +11,23 @@ router.get('/', async(req, res) => {
     console.log(err)
   }
 })
+
+// GET a drinking water fountain by specifying the ID
 router.get('/:id', async(req, res) => {
   try{
     const drinkingWaterLoc = await (Location.findById(req.params.id))
     res.send(drinkingWaterLoc)
+  } catch(err) {
+    console.log(err)
+  }
+})
+
+// Add a new drinking water fountain
+router.post('/new', async(req, res) => {
+  const location = new Location(req.body)
+  try{
+    const addedDrinkingWaterLoc = await (location.save())
+    res.json(addedDrinkingWaterLoc)
   } catch(err) {
     console.log(err)
   }
