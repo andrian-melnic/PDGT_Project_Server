f406ddc3522d6a5223138e7231450f6326119910
andrianMelnic29 <melnic.andry97@gmail.com>
2020-04-02 17:27:19 +0200

    Update app.js
    - Setted 'Access-Control-Allow-Methods'
    header for DELETE & PUT requests.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ require('./config/passport')
 const app = express()
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200)
